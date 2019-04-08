1139f3ee38ff324e5fb4184844104143f12b0dad
satej soman <satej.soman@gmail.com>
2019-04-07 23:55:19 -0500

    add spatial join implementation

--- hw1/code/hw1.py
+++ hw1/code/hw1.py
@@ -1,5 +1,6 @@
 #!python3
 
+import json
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -7,7 +8,9 @@ import numpy as np
 import pandas as pd
 import requests
 from matplotlib2tikz import save as tikz_save
-
+from tqdm import tqdm
+import geopandas as gpd
+from shapely.geometry import Point
 
 def get_cdp_data():
     # download crime data if we don't have it locally
@@ -67,7 +70,39 @@ def summarize(crime_stats):
         # tikz_save("../latex/" + label + ".tex", figurewidth = "2.5in", figureheight = "2.5in")
     # plt.show()
 
+# def geocode_row(row):
+#     url = "https://geocoding.geo.census.gov/geocoder/geographies/coordinates?x={}&y={}&benchmark=4&vintage=410&format=json".format(row["Longitude"], row["Latitude"])
+#     response = json.loads(requests.get(url).content)
+#     geographies = response["result"]["geographies"]
+#     return (geographies["Census Blocks"][0]["BLOCK"], geographies["Census Tracts"][0]["NAME"])
+
+# def geocode(crime_stats):
+#     geocoded_data_path = Path("./geocoded_crime_stats.csv")
+#     if not geocoded_data_path.exists():
+#         print("geocoding points")
+#         # restrict geocoding to valid locations
+#         crime_stats = crime_stats[crime_stats["Location"].notna()]
+#         crime_stats["census_block_data"] = crime_stats.progress_apply(geocode_row, axis=1)
+#         crime_stats.to_csv(geocoded_data_path)
+#         return crime_stats
+#     else: 
+#         return pd.read_csv(geocoded_data_path)
+
+def assign_census_tracts(crime_stats):
+    boundary_shp = "./Boundaries - Census Blocks - 2000/geo_export_8e9f6d85-3c5b-429f-b625-25afcc3dea85.shp"
+    census_tracts = gpd.read_file(boundary_shp).drop(columns=['perimeter', 'shape_area', 'shape_len'])
+    # restrict geocoding to valid locations
+    crime_stats = crime_stats[crime_stats["Location"].notna()]
+    crime_stats["geometry"] = crime_stats.apply(lambda row: Point(row["Longitude"], row["Latitude"]), axis = 1)
+    return gpd.tools.sjoin(gpd.GeoDataFrame(crime_stats), census_tracts, how="inner")
+
+def analyze_ward_43(crime_stats):
+    crime_stats = crime_stats[crime_stats["Ward"] == 43]
+    time_period_1
 
 if __name__ == "__main__":
+    tqdm.pandas()
     crime_stats = get_cdp_data()
     summarize(crime_stats)
+    geo_crime_stats = assign_census_tracts(crime_stats)
+
